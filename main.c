bf7a49e970c6d262ad211edcd148d232d269d46e
Your Name <you@example.com>
2024-12-20 17:57:48 +0800

    Running on K1 F3 Banana
    time taken for C implementation: 0.003515
    time taken for vector implementation: 0.002513

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#include <sys/time.h>
+#include <time.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -16,17 +18,43 @@ void vec_len(float *r, struct pt *v, int n){
   }
 }
 
-#define N 6
-struct pt v[N] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}, {13, 14, 15}, {16, 17, 18}};
+#define N 100000
 
 int main(){
   float lens[N], lens_rvv[N];
-  
-  vec_len(lens, v, N);
-  vec_len_rvv(lens_rvv, v, N);
+  srand(time(NULL));   // Initialization, should only be called once.
+  struct pt v[N];
+  time_t t = time(NULL);
+  struct tm tm = *localtime(&t);
+  printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
+  int r0 = 0;
+  int r1 = 0;
+  int r2 = 0;
   for (int i=0; i<N; ++i){
-    printf("%f %f\n", lens[i], lens_rvv[i]);
+    v[i].x = rand();
+    v[i].y = rand();
+    v[i].z = rand();
   }
+  struct timeval start, stop;
+  double secs = 0;
+  gettimeofday(&start, NULL);
+  vec_len(lens, v, N);
+  gettimeofday(&stop, NULL);
+  secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+  printf("time taken for C implementation: %f\n",secs);  
+  // printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+  vec_len_rvv(lens_rvv, v, N);
+  gettimeofday(&start, NULL);
+  secs = (double)(start.tv_usec - stop.tv_usec) / 1000000 + (double)(start.tv_sec - stop.tv_sec);
+  printf("time taken for vector implementation: %f\n",secs);  
+  //printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+  
+  for (int i=0; i<N; ++i){
+    //printf("%f %f\n", lens[i], lens_rvv[i]);
+  }
+  
+  tm = *localtime(&t);
+  //printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
   return 0;
 }
--- makefile
+++ makefile
@@ -2,4 +2,4 @@ go: main
 	qemu-riscv64 -cpu rv64,v=true,zba=true,vlen=128,rvv_ta_all_1s=on,rvv_ma_all_1s=on ./main
 
 main: main.c vec.S makefile
-	riscv64-unknown-elf-gcc -O main.c vec.S -o main -march=rv64gcv_zba -lm
+	riscv64-linux-gnu-gcc -O main.c vec.S -o main -march=rv64gcv_zba -lm
